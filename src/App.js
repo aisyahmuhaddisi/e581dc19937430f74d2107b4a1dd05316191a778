ee2a9c59949306d19a9385f72c2417f9409c02c1
Aisyah Muhaddisi <aisyahmuhaddisi@yahoo.com>
2019-09-13 00:56:54 +0700

    add bootstrap navbar and card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/navbar";
+import FoodCard from "./components/FoodCard";
+// import FoodList from './components/FoodList'
+// import RecipeDetails from './components/RecipeDetails'
+
 
 function App() {
+  // const url = `https://api.myjson.com/bins/t7szj`
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <FoodCard/>
     </div>
   );
 }
